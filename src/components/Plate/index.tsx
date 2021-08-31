0cf250047e307c7760910ce7d40e55c57fd43a2e
RennanD <rplaykl@gmail.com>
2021-08-30 22:53:13 -0300

    chore: animete plate

--- src/components/Plate/index.tsx
+++ src/components/Plate/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { Image } from 'react-native';
+
+import { MotiImage } from 'moti'
 
 import { styles } from './styles';
+
 import plateImg from '../../assets/plate.png';
 
 export function Plate() {
   return (
-    <Image
+    <MotiImage
       style={styles.plate}
       source={plateImg}
       resizeMode="contain"
+      from={{
+        opacity: 0,
+        rotate: '100deg'
+      }}
+      animate={{
+        opacity: 1,
+        rotate: '0deg'
+      }}
+      transition = {{
+        type: 'timing',
+        duration: 2000
+      }}
     />
   );
 }
